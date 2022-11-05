79f0a5e7803bb967a99c2774ea047c8b797cf2c0
Gnosis <gnosis@Gnosis-MacBook-Pro.local>
2022-11-05 01:04:04 +0100

    commit changes

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -84,13 +84,13 @@
                             <div class="d-sm-flex">
                                 <div class="ms-sm-2 me-sm-4 py-2">
                                     {{--                                    bg-body-dark--}}
-                                    <a class="item item-rounded  text-dark fs-2 mb-2 mx-auto" href="javascript:void(0)">
+                                    <a class="item item-rounded  text-dark fs-2 mb-2 mx-auto" href="{{ route('user.campaign') }}">
                                         {{--                                        <i class="fab fa-fw fa-facebook"></i>--}}
                                         <img height="70" width="70" src="{{ asset('files/'.$item->image ) }}" alt="">
                                     </a>
                                 </div>
                                 <div class="py-2">
-                                    <a class="link-fx h4 mb-1 d-inline-block text-dark" href="be_pages_jobs_listing.html">
+                                    <a class="link-fx h4 mb-1 d-inline-block text-dark" href="{{ route('user.campaign') }}">
                                         {{ $item->name }}
                                     </a>
 
