b28f9a981c78ecc34ea6532a105bf7f11522e465
benny <benny@benny-optiplex-7060>
2022-12-21 11:05:42 +0100

    implement referral system, added paystack callback and fund transactions store

--- database/migrations/2022_12_21_100418_add_ref_to_user.php
+++ database/migrations/2022_12_21_100418_add_ref_to_user.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddExtraFieldToUser extends Migration
+class AddRefToUser extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,8 @@ class AddExtraFieldToUser extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('country');
+            $table->unsignedBigInteger('referrer_id')->nullable()->after('balance');
+            $table->decimal('ref_bonus', 12,2)->default(0)->after('balance');
         });
     }
 
@@ -26,7 +27,7 @@ class AddExtraFieldToUser extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('country');
+            $table->dropColumn('referrer_id','ref_bonus');
         });
     }
 }
